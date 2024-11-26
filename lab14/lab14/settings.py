403c7200fff746daae5107b2f0f42b13551660a4
andre <andresalexanderhq@gmail.com>
2024-11-26 02:24:56 -0500

    apps principales del proyecto

--- lab14/lab14/settings.py
+++ lab14/lab14/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'usuarios',
+    'clientes',
+    'proveedores',
+    'facturas',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
